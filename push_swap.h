17ade10b85338ff54bf500f80ef6b98c85f6e0be
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-03-09 18:55:17 +0100

    testing files

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 14:50:31 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/03/07 14:51:38 by mghalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+# include <fcntl.h>
+# include <math.h>
+
+#endif
