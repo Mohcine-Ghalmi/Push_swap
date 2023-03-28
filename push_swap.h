545bc0568a12ebbbed3637539a4cecb17327b5eb
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-03-28 00:59:40 +0000

    still in test

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:50:31 by mghalmi           #+#    #+#             */
-/*   Updated: 2023/03/26 17:47:53 by mghalmi          ###   ########.fr       */
+/*   Updated: 2023/03/28 00:03:14 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,6 @@
 # include <fcntl.h>
 # include <math.h>
 # include "libft/libft.h"
-
-typedef struct stack
-{
-	long	nbr;
-	int		pos;
-	struct	t_stack	*coming;
-}   t_stack;
 
 int readingData(char **argv);
 
--- test.c
+++ test.c
@@ -25,18 +25,27 @@ int main(int argc, char **argv)
 	int i;
 	char **split;
 	char *join;
+	t_list *dataStart;
 	
 	i = 1;
 	join = ft_calloc(1, sizeof(char));
 	if (argc >= 2)
 	{
+		dataStart = NULL;
 		while (argv[i])
 			join = ft_strjoin(join, argv[i++], ' ');
 		split = ft_split(join, ' ');
 		check(split);
+		i = -1;
+		while (split[++i])
+			ft_lstadd_back(&dataStart, ft_lstnew(ft_atoi(split[i]), i));
 		i = 0;
-		while (split[i])
-			ft_printf("%d\n", ft_atoi(split[i++]));
+		while (dataStart)
+		{
+			printf("value = %ld || position =  %d\n", dataStart->content, dataStart->pos);
+			dataStart = dataStart->next;
+		}
 	}else
+		ft_printf("----idk fih----\n");
 	return (0);
 }
